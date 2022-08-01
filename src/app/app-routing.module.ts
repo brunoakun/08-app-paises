e5b2d5c35b6e598c342d542a69dfefd7cb4455e8
Bruno Barange <bruno.akun@gmail.com>
2022-08-02 00:07:37 +0200

    Crear router - Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { VerPaisComponent } from './paises/pages/ver-pais/ver-pais.component';
+import { PorCapitalComponent } from './paises/pages/por-capital/por-capital.component';
+import { PorRegionComponent } from './paises/pages/por-region/por-region.component';
+import { PorPaisComponent } from './paises/pages/por-pais/por-pais.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PorPaisComponent, pathMatch: 'full' },
+  { path: 'region', component: PorRegionComponent },
+  { path: 'pais', component: PorPaisComponent },
+  { path: 'capital', component: PorCapitalComponent },
+  { path: 'pais/:id', component: VerPaisComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
